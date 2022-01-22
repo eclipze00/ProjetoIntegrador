9a873278594f4afe0758aca4b96ce8e7bf46ed15
Wesley Barreto <94858339+Quelann@users.noreply.github.com>
2022-01-22 18:06:16 -0300

    Update TiposRepository.java

--- genTour/src/main/java/org/gentTour/genTour/TiposRepository/TiposRepository.java
+++ genTour/src/main/java/org/gentTour/genTour/TiposRepository/TiposRepository.java
@@ -1,10 +1,9 @@
 package org.gentTour.genTour.TiposRepository;
 /*
- * 
  * @author Wesley Barreto Coelho
  * @since 22/01/2022
  * @version 0.01
-  */
+ */
 import java.util.List;
 
 import org.gentTour.genTour.model.Tipos;
