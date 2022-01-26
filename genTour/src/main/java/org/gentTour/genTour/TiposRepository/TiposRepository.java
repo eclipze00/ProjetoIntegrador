ff72860cc83b02007eea18e0f37f997c8f748e3d
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.com>
2022-01-26 09:38:35 -0300

    @eclipze00 [Task: Add New Repository]

--- genTour/src/main/java/org/gentTour/genTour/TiposRepository/TiposRepository.java
+++ genTour/src/main/java/org/gentTour/genTour/TiposRepository/TiposRepository.java
@@ -1,6 +1,6 @@
 package org.gentTour.genTour.TiposRepository;
+
 /*
- * 
  * @author Wesley Barreto Coelho
  * @since 22/01/2022
  * @version 0.01
@@ -12,7 +12,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TiposRepository extends JpaRepository<Tipos, Long>{
+public interface TiposRepository extends JpaRepository<Tipos, Long> {
+
+	public List<Tipos> findAllByTipoTurismoContainingIgnoreCase(String tipoTurismo);
+
+	/*
+	 * @author Guilherme Barbosa Rodrigues
+	 * @since 26/01/2022
+	 * @version 0.03
+	 * 
+	 */
+	public List<Tipos> findAllByLocalContainingIgnoreCase(String local);
 	
-	public List<Tipos> findAllByTipoTurismoContainingIgnoreCase (String tipoTurismo);
+	public List<Tipos> findAllByTemporadaContainingIgnoreCase(String temporada);
+
 }
