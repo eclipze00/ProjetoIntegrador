4a650359585c07c948a148837d587da13ad2994c
Wesley Barreto <wesley_barreto@outlook.com.br>
2022-02-02 16:20:33 -0300

    @Quelann bugfix

--- genTour/src/main/java/org/gentTour/genTour/security/UserDetailsServiceImpl.java
+++ genTour/src/main/java/org/gentTour/genTour/security/UserDetailsServiceImpl.java
@@ -14,9 +14,7 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
 	private UsuarioRepository userRepository;
-	
-	
-	
+		
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 		
